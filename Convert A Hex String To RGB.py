5eb90f547a0cfbb3ed5215e911c5c80cb5cc37fc
Kinetic-Blast <87279302+Kinetic-Blast@users.noreply.github.com>
2023-11-29 09:28:10 -0600

    Update Convert A Hex String To RGB.py

--- Convert A Hex String To RGB.py
+++ Convert A Hex String To RGB.py
@@ -1,2 +1,16 @@
+"""
+When working with color values it can sometimes be useful to extract the individual red, green, and blue (RGB) component values for a color. Implement a function that meets these requirements:
+
+Accepts a case-insensitive hexadecimal color string as its parameter (ex. "#FF9933" or "#ff9933")
+Returns a Map<String, int> with the structure {r: 255, g: 153, b: 51} where r, g, and b range from 0 through 255
+Note: your implementation does not need to support the shorthand form of hexadecimal notation (ie "#FFF")
+
+Example
+"#FF9933" --> {r: 255, g: 153, b: 51}
+
+"""
+
+
+
 def hex_string_to_RGB(hex_s): 
     return {'r': int(hex_s[1:3],16), 'g': int(hex_s[3:5],16), 'b': int(hex_s[5:7],16)}
