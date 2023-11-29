7e179d5af9d3a63e174d545c63715b5c65bbd3c5
Kinetic-Blast <87279302+Kinetic-Blast@users.noreply.github.com>
2023-11-29 09:31:28 -0600

    Update Squirrel And Tree.py

--- Squirrel And Tree.py
+++ Squirrel And Tree.py
@@ -1,8 +1,11 @@
 '''
 
-Since the weather was good, some students decided to go for a walk in the park after the first lectures of the new academic year. There they saw a squirrel, which climbed a tree in a spiral at a constant angle to the ground. They calculated that in one loop the squirrel climbes h meters (vertical height), the height of the tree is equal to H (v in Ruby), and the length of its circumference equals S.
+Since the weather was good, some students decided to go for a walk in the park after the first lectures of the new academic year. 
+There they saw a squirrel, which climbed a tree in a spiral at a constant angle to the ground. 
+They calculated that in one loop the squirrel climbes h meters (vertical height), the height of the tree is equal to H (v in Ruby), and the length of its circumference equals S.
 
-These calculations were exhausting, so now the students need your help to figure out how many meters the path length of squirrel climbed when it reached the tree top. The result should be rounded to 4 decimal places.
+These calculations were exhausting, so now the students need your help to figure out how many meters the path length of squirrel climbed when it reached the tree top. 
+The result should be rounded to 4 decimal places.
 
 Example
 For h = 4, H = 16, S = 3, the output should be 20.
